d553e325643992e77000ea98ef12d821ae90a1e7
robtfm <50659922+robtfm@users.noreply.github.com>
2022-07-25 12:11:56 +0100

    more things

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![feature(let_else)]
-use std::{collections::HashMap, hash::Hash};
 use glam::UVec3;
+use std::{collections::HashMap, hash::Hash};
 
 #[cfg(test)]
 mod tests {
-    use glam::UVec3;
     use crate::{AtlasPage, Slot};
+    use glam::UVec3;
 
     #[test]
     fn insert_stuff() {
@@ -19,16 +19,22 @@ mod tests {
         assert_eq!(page.insert(h0, UVec3::splat(6)), Slot::New(UVec3::ZERO));
 
         // inserting again gets same
-        assert_eq!(page.insert(h0, UVec3::splat(6)), Slot::Existing(UVec3::ZERO));
+        assert_eq!(
+            page.insert(h0, UVec3::splat(6)),
+            Slot::Existing(UVec3::ZERO)
+        );
 
         // second item doesn't fit
         assert_eq!(page.insert(h1, UVec3::splat(6)), Slot::NoFit);
 
         // smaller item fits right
-        assert_eq!(page.insert(h2, UVec3::splat(4)), Slot::New(UVec3::new(6, 0, 0)));
+        assert_eq!(
+            page.insert(h2, UVec3::splat(4)),
+            Slot::New(UVec3::new(6, 0, 0))
+        );
 
         // second item fits after removal
-        page.remove(h0);
+        page.remove(&h0);
         assert_eq!(page.insert(h1, UVec3::splat(6)), Slot::New(UVec3::ZERO));
 
         // first item no longer fits
@@ -38,9 +44,23 @@ mod tests {
         page.insert(h0, UVec3::splat(2));
         let Slot::New(pos) = page.insert(h1, UVec3::splat(2)) else {panic!()};
         page.insert(h2, UVec3::splat(2));
-        page.remove(h1);
+        page.remove(&h1);
         // reinsert gets original location if not paged out
         assert_eq!(page.insert(h1, UVec3::splat(2)), Slot::Existing(pos))
+    }
+
+    #[test]
+    fn check_lru() {
+        let mut page = AtlasPage::new(UVec3::splat(10));
+
+        let h0 = 0;
+        let h1 = 1;
+
+        page.insert(h1, UVec3::ONE);
+        page.remove(&h1);
+
+        assert_eq!(page.insert(h0, UVec3::ONE), Slot::New(UVec3::X));
+        assert_eq!(page.insert(h1, UVec3::ONE), Slot::Existing(UVec3::ZERO));
     }
 }
 
@@ -62,7 +82,7 @@ pub enum Slot {
 
 #[derive(Clone)]
 pub struct AtlasPage<H: Eq + Hash + Clone> {
-    dim: UVec3,
+    pub dim: UVec3,
     live_items: HashMap<H, AtlasInfo>,
     dead_items: HashMap<H, AtlasInfo>,
 }
@@ -88,7 +108,6 @@ impl<H: Eq + Hash + Clone> AtlasPage<H> {
         let mut distance = self.dim - pos - size;
         let mut to_clear = Vec::new();
 
-
         // check for intersections with live items
         for current_item in self.live_items.values() {
             let cur_lhs = current_item.position;
@@ -97,10 +116,10 @@ impl<H: Eq + Hash + Clone> AtlasPage<H> {
             let intersects = new_lhs.cmplt(cur_rhs) & new_rhs.cmpgt(cur_lhs);
 
             if intersects.all() {
-                return None
+                return None;
             }
 
-            if intersects.y && intersects.z && cur_lhs.x > new_rhs.x { 
+            if intersects.y && intersects.z && cur_lhs.x > new_rhs.x {
                 let distance_x = cur_lhs.x - new_rhs.x;
                 if distance_x < distance.x {
                     distance.x = distance_x;
@@ -150,16 +169,30 @@ impl<H: Eq + Hash + Clone> AtlasPage<H> {
             return Slot::Existing(info.position);
         }
 
-        let (mut best_point, mut best_distance, mut best_evict_count, mut evictions) = (None, u32::MAX, usize::MAX, Vec::new());
+        let (mut best_point, mut best_distance, mut best_evict_count, mut evictions) =
+            (None, u32::MAX, usize::MAX, Vec::new());
 
-        let mut insert_points = vec!(UVec3::ZERO);
+        let mut insert_points = vec![UVec3::ZERO];
         for item in self.live_items.values() {
-            insert_points.extend([item.position + item.size * UVec3::X, item.position + item.size * UVec3::Y, item.position + item.size * UVec3::Z]);
+            insert_points.extend([
+                item.position + item.size * UVec3::X,
+                item.position + item.size * UVec3::Y,
+                item.position + item.size * UVec3::Z,
+            ]);
+        }
+        for item in self.dead_items.values() {
+            insert_points.extend([
+                item.position + item.size * UVec3::X,
+                item.position + item.size * UVec3::Y,
+                item.position + item.size * UVec3::Z,
+            ]);
         }
 
         for insert_point in insert_points {
             if let Some((insert_distance, insert_evictions)) = self.measure(insert_point, size) {
-                if insert_evictions.len() < best_evict_count || insert_evictions.len() == best_evict_count && insert_distance < best_distance {
+                if insert_evictions.len() < best_evict_count
+                    || insert_evictions.len() == best_evict_count && insert_distance < best_distance
+                {
                     best_point = Some(insert_point);
                     best_distance = insert_distance;
                     best_evict_count = insert_evictions.len();
@@ -170,36 +203,42 @@ impl<H: Eq + Hash + Clone> AtlasPage<H> {
 
         match best_point {
             Some(position) => {
-                self.live_items.insert(handle, AtlasInfo{ size, position });
+                self.live_items.insert(handle, AtlasInfo { size, position });
                 for item in evictions {
                     self.dead_items.remove(&item);
                 }
 
                 Slot::New(position)
-            },
+            }
             None => Slot::NoFit,
         }
     }
 
-    pub fn get(&self, handle: H) -> Option<AtlasInfo> {
-        self.live_items.get(&handle).copied()
+    pub fn get(&self, handle: &H) -> Option<AtlasInfo> {
+        self.live_items.get(handle).copied()
     }
 
     // mark as dead, keep around in case it gets added back
-    pub fn remove(&mut self, handle: H) {
-        if let Some(info) = self.live_items.remove(&handle) {
-            self.dead_items.insert(handle, info);
+    pub fn remove(&mut self, handle: &H) {
+        if let Some((key, info)) = self.live_items.remove_entry(handle) {
+            self.dead_items.insert(key, info);
         }
     }
 
     // remove without keeping in reserve
-    pub fn purge(&mut self, handle: H) {
-        self.live_items.remove(&handle);
-        self.dead_items.remove(&handle);
+    pub fn purge(&mut self, handle: &H) {
+        self.live_items.remove(handle);
+        self.dead_items.remove(handle);
     }
 
     // mark all handles as dead
-    pub fn clear(&mut self) {
+    pub fn remove_all(&mut self) {
         self.dead_items.extend(self.live_items.drain())
+    }
+
+    // purge all
+    pub fn purge_all(&mut self) {
+        self.live_items.clear();
+        self.dead_items.clear();
     }
 }
